42fe83b6323eba06a94897075c068e34226d9cc4
Joel Lijo Mathew <joellijo4@gmail.com>
2025-03-09 17:06:55 +0530

    Added a del button to delete the last entered character.

--- script/script.js
+++ script/script.js
@@ -1,6 +1,14 @@
 function btnClicked(val){
-    document.getElementById("output").value +=val
-    
+    if(val=="/" || val=="*" || val=="+" || val=="-"){
+        document.getElementById("output").value += " " + val + " "
+    }else{
+        document.getElementById("output").value += val
+    }
+}
+
+function del(){
+    var string = document.getElementById("output").value
+    document.getElementById("output").value = string.substr(0, string.length-1)
 }
 
 function clr(){
@@ -9,6 +17,9 @@ function clr(){
 
 function equalTo(){
     var expsn = document.getElementById("output").value 
+    if(expsn == ""){
+        return
+    }
     var result = eval(expsn)
     document.getElementById("output").value = result 
 }
