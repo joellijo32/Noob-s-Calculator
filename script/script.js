638efc881fa47cb3c3691a74d0e2ce46ae0fe962
Joel Lijo Mathew <joellijo4@gmail.com>
2025-03-10 10:31:34 +0530

    Minor changes, forgot to mention

--- script/script.js
+++ script/script.js
@@ -1,25 +1,26 @@
-function btnClicked(val){
-    if(val=="/" || val=="*" || val=="+" || val=="-"){
-        document.getElementById("output").value += " " + val + " "
-    }else{
-        document.getElementById("output").value += val
-    }
+// Created by Joel Lijo Mathew
+function btnClicked(val) {
+  if (val == "/" || val == "*" || val == "+" || val == "-") {
+    document.getElementById("output").value += " " + val + " ";
+  } else {
+    document.getElementById("output").value += val;
+  }
 }
 
-function del(){
-    var string = document.getElementById("output").value
-    document.getElementById("output").value = string.substr(0, string.length-1)
+function del() {
+  var string = document.getElementById("output").value;
+  document.getElementById("output").value = string.substr(0, string.length - 1);
 }
 
-function clr(){
-    document.getElementById("output").value= ""
+function clr() {
+  document.getElementById("output").value = "";
 }
 
-function equalTo(){
-    var expsn = document.getElementById("output").value 
-    if(expsn == ""){
-        return
-    }
-    var result = eval(expsn)
-    document.getElementById("output").value = result 
+function equalTo() {
+  var expsn = document.getElementById("output").value;
+  if (expsn == "") {
+    return;
+  }
+  var result = eval(expsn);
+  document.getElementById("output").value = result;
 }
