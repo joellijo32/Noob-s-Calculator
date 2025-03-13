b37ca66bb7dcc906f66a19437167a527260df604
Joel Lijo Mathew <joellijo4@gmail.com>
2025-03-13 07:30:47 +0530

    Added keyboard input for more interactive feeling, rather than cursor clicking buttons

--- script/script.js
+++ script/script.js
@@ -1,4 +1,45 @@
 // Created by Joel Lijo Mathew
+
+window.addEventListener("keydown", function (event) {
+  event.preventDefault();
+  if (
+    event.key == "/" ||
+    event.key == "*" ||
+    event.key == "+" ||
+    event.key == "-"
+  ) {
+    playAudio();
+
+    document.getElementById("output").value += " " + event.key + " ";
+  } else {
+    if (event.key == "Enter") {
+      
+      equalTo();
+    } else {
+      if (event.key == "Backspace" || event.key == "Delete") {
+        
+        del();
+      } else {
+        if (event.key == " ") {
+          clr();
+        } else {
+          if (
+            (event.key >= "0" && event.key <= "9") ||
+            event.key == ")" ||
+            event.key == "("
+          ){
+            playAudio();
+          document.getElementById("output").value += event.key;
+          }
+          else{
+            return;
+          }
+        }
+      }
+    }
+  }
+});
+
 function btnClicked(val) {
   if (val == "/" || val == "*" || val == "+" || val == "-") {
     document.getElementById("output").value += " " + val + " ";
@@ -10,13 +51,16 @@ function btnClicked(val) {
 function del() {
   var string = document.getElementById("output").value;
   document.getElementById("output").value = string.substr(0, string.length - 1);
+  playAudio();
 }
 
 function clr() {
+  playAudio();
   document.getElementById("output").value = "";
 }
 
 function equalTo() {
+  playAudio();
   var expsn = document.getElementById("output").value;
   if (expsn == "") {
     return;
